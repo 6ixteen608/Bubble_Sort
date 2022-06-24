1e347e96a27d4cb1861c18af5759958cd4f40734
cepew6ix <cepew6ix@gmail.com>
2022-06-24 10:09:23 +0600

    test modify

--- TestProject1/UnitTest1.cs
+++ TestProject1/UnitTest1.cs
@@ -8,19 +8,19 @@ namespace TestProject1
         public void TestMethod1()
         {
             int[] someArray = new int[] { 9, 4, 8, 7, 5, 6, 2, 1, 3, 0 };
+            int[] someArray2 = new int[] { 4, 9, 8, 7, 6, 5, 2, 1, 3, 0 };
+            int[] someArray3 = new int[] { 9, 8, 4, 7, 5, 6, 1, 2, 3, 0 };
+            int[] someArray4 = new int[] { 0, 4, 8, 5, 7, 6, 2, 1, 3, 9 };
             int[] expectedArray = new int[] { 1, 2, 4, 3, 8, 5, 7, 6, 9, 0 };
             MassSort.Bubble_Sort(someArray);
-            int k = 0;
-            int j;
-            for (j = 0; j < someArray.Length ; j++)
-            {
-                //Если элемент массива с индексом j больше следующего за ним элемента
-                if (someArray[j] == expectedArray[j])
-                {
-                    k++;
-                }
-            }
-            Assert.AreEqual(k, j);
+            MassSort.Bubble_Sort(someArray2);
+            MassSort.Bubble_Sort(someArray3);
+            MassSort.Bubble_Sort(someArray4);
+            Assert.AreEqual(someArray, expectedArray);
+            Assert.AreEqual(someArray2, expectedArray);
+            Assert.AreEqual(someArray3, expectedArray);
+            Assert.AreEqual(someArray4, expectedArray);
+
         }
     }
 }
